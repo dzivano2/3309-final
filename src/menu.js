5fbcefdb1c2dd71f0f2da560f2ad5b8b222ea4ca
MalcolmFonseca <mfonsec4@uwo.ca>
2023-12-06 11:36:16 -0500

    menu added

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./menu.css";
 
 //complex function is changing price of all items that contain specific ingredient
 // Sample menu items data (replace with your own data)
@@ -13,28 +14,41 @@ const Menu = () => {
   // State to manage the menu items data
   const [menuItems, setMenuItems] = useState(initialMenuItems);
 
+  //store user inputs
+  const [ingredient, setIngredient] = useState("");
+  const [change, setChange] = useState(0);
+
+  function handleIncrease() {}
+
   return (
     <div>
-      <h2>Menu</h2>
-
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {menuItems.map((menuItem) => (
-            <tr key={menuItem.id}>
-              <td>{menuItem.id}</td>
-              <td>{menuItem.name}</td>
-              <td>${menuItem.price.toFixed(2)}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <h1>Menu</h1>
+      <div id="itemFunction">
+        <form onSubmit={handleIncrease}>
+          <label>Increase price of dishes that contain: </label>
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="Ingredient"
+            onChange={(e) => setIngredient(e.target.value)}
+          ></input>
+          <label>By: </label>
+          <input
+            type="number"
+            className="inputBox"
+            placeholder="Change"
+            onChange={(e) => setChange(e.target.value)}
+          ></input>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      {menuItems.map((menuItem) => (
+        <div className="menuItem" key={menuItem.id}>
+          <h2 className="itemid">{menuItem.id}</h2>
+          <h3 className="itemName">{menuItem.name}</h3>
+          <h3 className="itemPrice">${menuItem.price.toFixed(2)}</h3>
+        </div>
+      ))}
     </div>
   );
 };
