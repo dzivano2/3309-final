ddeda31d91104bb00fecf9254c5a32fca93bb772
Sajon <sumaibal@uwo.ca>
2023-12-06 16:24:02 -0500

    backend

--- server/server.js
+++ server/server.js
@@ -4,12 +4,26 @@ const mysql = require("mysql");
 const app = express();
 app.use(express.json());
 
+let cors = require("cors");
+app.use(cors());
+
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword",
-  database: "restaurantDB" 
+  host: "127.0.0.1",
+  user: "root",
+  password: "password",
+  database: "restaurantdb",
+  port: '4000'
 });
+
+const dbConfig = {
+  host: "127.0.0.1",
+  user: "root",
+  password: "password",
+  database: "restaurantdb",
+  port: '4000'
+};
+
+const pool = mysql.createPool(dbConfig);
 
 con.connect(function (err) {
   if (err) {
@@ -21,5 +35,72 @@ con.connect(function (err) {
 
 
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4001;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.get('/api/reservations', (req, res) => {
+  const query = `
+    SELECT 
+      r.ReservationID AS id,  
+      CONCAT(c.FirstName, ' ', c.LastName) AS customerName, 
+      DATE_FORMAT(r.Time, '%Y-%m-%d') AS date,  
+      TIME_FORMAT(r.Time, "%H:%i") AS time,  
+      r.NumberOfPatrons AS numberOfPeople, 
+      r.TableNumber AS tableNumber
+    FROM Reservation r
+    JOIN Customer c ON r.CustomerID = c.CustomerID
+  `;
+
+  pool.query(query, (error, results) => {
+    if (error) {
+      console.error('Error executing the query:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+
+    res.json(results);
+  });
+});
+
+app.post('/processReservation', (req, res) => {
+  const {
+    customerName,
+    date,
+    time,
+    numberOfPeople,
+    tableNumber,
+  } = req.body;
+
+  
+
+
+  var [firstName, lastName = '1'] = customerName.split(' ');
+  const phone = '1111111';
+
+  const rand = Math.floor(Math.random() * (8000 - 3000 + 1)) + 3000;
+
+
+  const insertCustomerQuery = 'INSERT INTO Customer (CustomerID, FirstName, LastName, PhoneNumber) VALUES (?, ?, ?, ?)';
+  pool.query(insertCustomerQuery, [rand, firstName, lastName, phone], (error, results, fields) => {
+    if (error) throw error;
+
+    const customerId = rand;
+
+    const insertReservationQuery = 'INSERT INTO Reservation (ReservationID, CustomerID, Time, Status, NumberOfPatrons, TableNumber) VALUES (?, ?, ?, ?, ?, ?)';
+    pool.query(insertReservationQuery, [rand, customerId, `${date} ${time}`, 'Pending', numberOfPeople, tableNumber], (error, results, fields) => {
+      if (error) throw error;
+
+      const reservationId = rand;
+
+ 
+      res.json({
+        ReservationID: reservationId,
+        customerName: `${firstName} ${lastName}`,
+        date,
+        time,
+        numberOfPeople,
+        tableNumber,
+      });
+    });
+  });
+});
